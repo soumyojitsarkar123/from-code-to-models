7ddcea8977f6570cbe7402bfeb3e25147138d112
Soumyojit Sarkar <soumyojitsarkar688@gmail.com>
2026-05-21 23:43:22 +0530

    From here we will start our journey of code to models

---
+++ 1_python_fundamentals/1_python_fundamentals/hello.py
@@ -0,0 +1,17 @@
+'''
+
+let's start with first code snippet
+
+'''
+
+print("Hello World")
+
+print("From here we will start our journey of code to models")
+
+
+"""
+output : 
+Hello World
+From here we will start our journey of code to models
+
+"""
